8c75688ce68cdb7b38fd840b420f72ef26e4c756
Doom Goon <doomgoonstudio@Dooms-MacBook-Pro.local>
2018-03-24 22:50:58 -0700

    Updates CSS and added betting logic to JS

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 /*----- constants -----*/
 var symbols = [
-    { name: 'gun', imgUrl: '', val: 0 },
-    { name: 'boots', imgUrl: '', val: 5 },
-    { name: 'whiskey', imgUrl: '', val: 25 },
-    { name: 'wanted', imgUrl: '', val: 50 },
-    { name: 'horse', imgUrl: '', val: 100 },
-    { name: 'goldNug', imgUrl: '', val: 250 },
+    { name: 'gun', imgUrl: 'https://i.imgur.com/Wuslckp.jpg', val: 0 },
+    { name: 'badge', imgUrl: 'https://i.imgur.com/4zaXTbk.jpg', val: 5 },
+    { name: 'dice', imgUrl: 'https://i.imgur.com/No8D3HJ.jpg', val: 25 },
+    { name: 'horseshoe', imgUrl: 'https://i.imgur.com/hsQB8b0.jpg', val: 50 },
+    { name: 'whiskey', imgUrl: 'https://i.imgur.com/FlTithL.jpg', val: 100 },
+    { name: 'gold', imgUrl: 'https://i.imgur.com/vXaki6K.jpg', val: 250 },
 ];
 
 var weighting = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 2, 2, 2];
@@ -14,10 +14,13 @@ var weighting = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 2, 2, 2];
 let bet, cash, msg, win, reels;
 
 /*----- cached element references -----*/
-var slotReel = document.querySelectorAll('.reel');
+var slotReelA = document.querySelector('#a');
+var slotReelB = document.querySelector('#b');
+var slotReelC = document.querySelector('#c');
+
 var betEl = document.querySelector('#bet');
 var cshEl = document.querySelector('#cash');
-var msgEl = document.querySelector('#message h2')
+var msgEl = document.querySelector('#message h2');
 
 /*----- event listeners -----*/
 document.querySelector('#bet-btns').addEventListener('click', betting);
@@ -28,12 +31,43 @@ document.querySelector('#reset').addEventListener('click', initialize);
 
 initialize();
 
+function betting(evt) {
+    let placeBet = parseInt(evt.target.textContent);
+    console.log(placeBet);
+
+    if  (placeBet > cash){
+        return;
+    } else if (cash > 0) {
+        cash = (cash -= placeBet);
+        bet = (bet += placeBet);
+    }
+    
+    if (cash === 0) msg = 'You Feelin Lucky?'
+
+    render();
+
+    
+ //each bet decrease cash, win = bet + amount, lose just losing bet
+}
+
+function spin() {
+
+}
+
+function winner() {
+
+
+}
+
 function render() {
     //activete msg, slot reels, bet, money
-    betEl.textContent = 'bet:' + bet;
-    cshEl.textContent = '$:' + cash;
+    //render is used every time user interacts
+    betEl.textContent = 'Bet:' + ' $' + bet;
+    cshEl.textContent = 'Cash:' + ' $' + cash;
     msgEl.textContent = msg;
-    slotReel.style.backgroundImage = `url(${[].imgUrl})`;
+    slotReelA.style.backgroundImage = `url(${symbols[0].imgUrl})`;
+    slotReelB.style.backgroundImage = `url(${symbols[0].imgUrl})`;
+    slotReelC.style.backgroundImage = `url(${symbols[0].imgUrl})`;
 }
 
 function initialize() {
@@ -48,19 +82,5 @@ function initialize() {
     }
 
     render();
-}
-
-//render is used every time user interacts
-
-function betting(evt) {
-    parseInt(evt.target.innerText('$', ''));
-    console.log(evt);
-
-
-    //each bet decrease cash, win = bet + amount, lose just losing bet
-}
-
-function spin() {
-
 }
 
