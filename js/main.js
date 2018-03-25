960f78e29dcfd9e25dac0ef66d2f7d1b566043b7
Doom Goon <doomgoonstudio@Dooms-MacBook-Pro.local>
2018-03-25 12:04:22 -0700

    Finalized layout, added sound and blinking text

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
 /*----- constants -----*/
 var symbols = [
-    { name: 'gun', imgUrl: 'https://i.imgur.com/Wuslckp.jpg', val: 0 },
-    { name: 'badge', imgUrl: 'https://i.imgur.com/4zaXTbk.jpg', val: 5 },
-    { name: 'dice', imgUrl: 'https://i.imgur.com/No8D3HJ.jpg', val: 25 },
-    { name: 'horseshoe', imgUrl: 'https://i.imgur.com/hsQB8b0.jpg', val: 50 },
-    { name: 'whiskey', imgUrl: 'https://i.imgur.com/FlTithL.jpg', val: 100 },
-    { name: 'gold', imgUrl: 'https://i.imgur.com/vXaki6K.jpg', val: 250 },
+    { name: 'gun', imgUrl: 'https://i.imgur.com/YgvwBl9.jpg', val: 0 },
+    { name: 'badge', imgUrl: 'https://i.imgur.com/RsIytun.jpg', val: 5 },
+    { name: 'dice', imgUrl: 'https://i.imgur.com/0WqsBCk.jpg', val: 25 },
+    { name: 'horseshoe', imgUrl: 'https://i.imgur.com/Aagv5e7.jpg', val: 50 },
+    { name: 'whiskey', imgUrl: 'https://i.imgur.com/OTA99Wi.jpg', val: 100 },
+    { name: 'loot', imgUrl: 'https://i.imgur.com/8ga3OIM.jpg', val: 250 },
 ];
 
 var weighting = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 2, 2, 2];
+
+var sounds = {
+    landing: 'http://soundbible.com/mp3/Cowboy_Theme-Pavak-1711860633.mp3',
+    betting: 'http://freesound.org/data/previews/79/79172_1230147-lq.mp3',
+    resetting: 'http://freesound.org/data/previews/165/165390_2989529-lq.mp3',
+    letErRip: 'http://freesound.org/data/previews/160/160885_2895933-lq.mp3',
+    spinning: 'http://freesound.org/data/previews/69/69689_866625-lq.mp3',
+}
 
 /*----- app's state (variables) -----*/
 let bet, cash, msg, win, reels;
 
 /*----- cached element references -----*/
+var audio = document.getElementById("bgSound");
+
 var slotReelA = document.querySelector('#a');
 var slotReelB = document.querySelector('#b');
 var slotReelC = document.querySelector('#c');
@@ -30,24 +40,25 @@ document.querySelector('#reset').addEventListener('click', initialize);
 /*----- functions -----*/
 
 initialize();
+audio.volume = 0.2;
 
 function betting(evt) {
     let placeBet = parseInt(evt.target.textContent);
     console.log(placeBet);
 
-    if  (placeBet > cash){
+    if (placeBet > cash) {
         return;
     } else if (cash > 0) {
         cash = (cash -= placeBet);
         bet = (bet += placeBet);
     }
-    
+
     if (cash === 0) msg = 'You Feelin Lucky?'
 
     render();
 
-    
- //each bet decrease cash, win = bet + amount, lose just losing bet
+
+    //each bet decrease cash, win = bet + amount, lose just losing bet
 }
 
 function spin() {
@@ -71,7 +82,6 @@ function render() {
 }
 
 function initialize() {
-
     msg = 'Giddy Up Pardner';
     bet = 0;
     cash = 50;
@@ -83,4 +93,3 @@ function initialize() {
 
     render();
 }
-
